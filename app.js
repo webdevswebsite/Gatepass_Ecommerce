3bd6152f2d82fa83c64bc18429248849f1d6fd92
mayowa86 <mayowasamuel86@gmail.com>
2022-08-01 11:18:32 +0100

    update cart checkout product

--- app.js
+++ app.js
@@ -1,45 +1,44 @@
-const cartContainer = document.querySelector('.cart-container');
-const productList = document.querySelector('.product-list');
-const cartList = document.querySelector('.cart-list');
-const cartTotalValue = document.getElementById('cart-total-value');
-const cartCountInfo = document.getElementById('cart-count-info');
+const cartContainer = document.querySelector(".cart-container");
+const productList = document.querySelector(".product-list");
+const cartList = document.querySelector(".cart-list");
+const cartTotalValue = document.getElementById("cart-total-value");
+const cartCountInfo = document.getElementById("cart-count-info");
 let cartItemID = 1;
 
 eventListeners();
 
 // all event listeners
-function eventListeners(){
-    window.addEventListener('DOMContentLoaded', () => {
-        loadJSON();
-        loadCart();
-    });
+function eventListeners() {
+	window.addEventListener("DOMContentLoaded", () => {
+		loadJSON();
+		loadCart();
+	});
 
-    // add to cart
-    productList.addEventListener('click', purchaseProduct);
+	// add to cart
+	productList.addEventListener("click", purchaseProduct);
 
-    // delete from cart
-    cartList.addEventListener('click', deleteProduct);
+	// delete from cart
+	cartList.addEventListener("click", deleteProduct);
 }
 
-
 // update cart info
-function updateCartInfo(){
-    let cartInfo = findCartInfo();
-    console.log(cartInfo)
-    cartCountInfo.textContent = cartInfo.productCount;
-    cartTotalValue.textContent = cartInfo.total;
+function updateCartInfo() {
+	let cartInfo = findCartInfo();
+	console.log(cartInfo);
+	cartCountInfo.textContent = cartInfo.productCount;
+	cartTotalValue.textContent = cartInfo.total;
 }
 
 updateCartInfo();
 
 // load product items content form JSON file
-function loadJSON(){
-    fetch('products.json')
-    .then(response => response.json())
-    .then(data =>{
-        let html = '';
-        data.forEach(product => {
-             html += `
+function loadJSON() {
+	fetch("products.json")
+		.then((response) => response.json())
+		.then((data) => {
+			let html = "";
+			data.forEach((product) => {
+				html += `
           <div class = "product-item">
                     <div class = "product-img">
                         <img src = "${product.imgSrc}" alt = "product image">
@@ -52,15 +51,16 @@ function loadJSON(){
                         <h3 class = "product-name">${product.name}</h3>
                         <span class = "product-category">${product.category}</span>
                         <p class = "product-price">₦${product.price}</p>
+                        <p class = "product-price">${product.ratings}</p>
                     </div>
                 </div>
             `;
-        });
-        productList.innerHTML = html;
-    })
-    .catch(error => {
-        alert(`User live server or local server`);
-    })
+			});
+			productList.innerHTML = html;
+		})
+		.catch((error) => {
+			alert(`User live server or local server`);
+		});
 }
 
 // add alert to product added to cart
@@ -69,108 +69,110 @@ function loadJSON(){
 // }
 
 // purchase product
-function purchaseProduct(e){
-    if (e.target.classList.contains('add-to-cart-btn')) {
-        let product = e.target.parentElement.parentElement;
-        getProductInfo(product);
-    }
+function purchaseProduct(e) {
+	if (e.target.classList.contains("add-to-cart-btn")) {
+		let product = e.target.parentElement.parentElement;
+		getProductInfo(product);
+	}
 }
 
-function getProductInfo(product){
-    let productInfo = {
-        id: cartItemID,
-        imgSrc: product.querySelector('.product-img img').src,
-        name: product.querySelector('.product-name').textContent,
-        category: product.querySelector('.product-category').textContent,
-        price: product.querySelector('.product-price').textContent
-    }
-    cartItemID++;
-    addToCartList(productInfo);
-    saveProductInStorage(productInfo);
-    addProductAlert()
+function getProductInfo(product) {
+	let productInfo = {
+		id: cartItemID,
+		imgSrc: product.querySelector(".product-img img").src,
+		name: product.querySelector(".product-name").textContent,
+		category: product.querySelector(".product-category").textContent,
+		price: product.querySelector(".product-price").textContent,
+	};
+	cartItemID++;
+	addToCartList(productInfo);
+	saveProductInStorage(productInfo);
+	addProductAlert();
 }
 
 // add the selected product to the cart list
 
-function addToCartList(product){
-    const cartItem = document.createElement('div');
-    cartItem.classList.add('cart-item');
-    cartItem.setAttribute('data-id', `${product.id}`);
-    cartItem.innerHTML = `
+function addToCartList(product) {
+	const cartItem = document.createElement("div");
+	cartItem.classList.add("cart-item");
+	cartItem.setAttribute("data-id", `${product.id}`);
+	cartItem.innerHTML = `   
         <img src = "${product.imgSrc}" alt = "product image">
         <div class = "cart-item-info">
             <h3 class = "cart-item-name">${product.name}</h3>
-  
             <span class = "cart-item-price">${product.price}</span>
         </div>
 
         <button type = "button" class = "cart-item-del-btn">
-            <i class = "ti-close"></i>
-        </button>
+            <i class = "icon-close"></i>
+        </button>        
     `;
-    cartList.appendChild(cartItem);
+	cartList.appendChild(cartItem);
 }
 
 // save the product in the local storage
-function saveProductInStorage(item){
-    let products = getProductFromStorage();
-    products.push(item);
-    localStorage.setItem('products', JSON.stringify(products));
-    updateCartInfo();
+function saveProductInStorage(item) {
+	let products = getProductFromStorage();
+	products.push(item);
+	localStorage.setItem("products", JSON.stringify(products));
+	updateCartInfo();
 }
 
 // get all the products info if there is any in the local storage
-function getProductFromStorage(){
-    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+function getProductFromStorage() {
+	return localStorage.getItem("products")
+		? JSON.parse(localStorage.getItem("products"))
+		: [];
 }
 
 // load carts product
-function loadCart(){
-    let products = getProductFromStorage();
-    if(products.length < 1){
-        cartItemID = 1; 
-    } else {
-        cartItemID = products[products.length - 1].id;
-        cartItemID++;
-    }
-    products.forEach(product => addToCartList(product));
+function loadCart() {
+	let products = getProductFromStorage();
+	if (products.length < 1) {
+		cartItemID = 1;
+	} else {
+		cartItemID = products[products.length - 1].id;
+		cartItemID++;
+	}
+	products.forEach((product) => addToCartList(product));
 
-    // calculate and update UI of cart info 
-    updateCartInfo();
+	// calculate and update UI of cart info
+	updateCartInfo();
 }
 
 // calculate total price of the cart and other info
-function findCartInfo(){
-    let products = getProductFromStorage();
-    let total = products.reduce((acc, product) => {
-        let price = parseFloat(product.price.substr(1));
-        return acc += price;
-    }, 0); // adding all the prices
+function findCartInfo() {
+	let products = getProductFromStorage();
+	console.log(products);
+	let total = products.reduce((acc, product) => {
+		let price = parseFloat(product.price.substr(1));
+		return (acc += price);
+	}, 0); // adding all the prices
 
-    return{
-        total: total.toFixed(2),
-        productCount: products.length
-    }
+	return {
+		total: total.toFixed(2),
+		productCount: products.length,
+	};
 }
+console.log(productCount);
 
 // delete product from cart list and local storage
-function deleteProduct(e){
-    let cartItem;
-    if(e.target.tagName === "BUTTON"){
-        cartItem = e.target.parentElement;
-        cartItem.remove(); 
-    } else if(e.target.tagName === "I"){
-        cartItem = e.target.parentElement.parentElement;
-        cartItem.remove(); 
-    }
+function deleteProduct(e) {
+	let cartItem;
+	if (e.target.tagName === "BUTTON") {
+		cartItem = e.target.parentElement;
+		cartItem.remove();
+	} else if (e.target.tagName === "I") {
+		cartItem = e.target.parentElement.parentElement;
+		cartItem.remove();
+	}
 
-    let products = getProductFromStorage();
-    let updatedProducts = products.filter(product => {
-        return product.id !== parseInt(cartItem.dataset.id);
-    });
-    localStorage.setItem('products', JSON.stringify(updatedProducts)); 
-    updateCartInfo();
-    console.log(products)
-    console.log(updatedProducts)
-
+	let products = getProductFromStorage();
+	let updatedProducts = products.filter((product) => {
+		return product.id !== parseInt(cartItem.dataset.id);
+	});
+	localStorage.setItem("products", JSON.stringify(updatedProducts));
+	updateCartInfo();
+	console.log(products);
+	console.log(updatedProducts);
 }
